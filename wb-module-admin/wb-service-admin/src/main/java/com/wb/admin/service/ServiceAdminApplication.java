bae52042b2936cdc8e27d6e20fbeb09ec697a9ff
wangbo <169966586@qq.com>
2021-03-05 17:10:12 +0800

    add wb-module-admin project

---
+++ wb-module-admin/wb-service-admin/src/main/java/com/wb/admin/service/ServiceAdminApplication.java
@@ -0,0 +1,20 @@
+package com.wb.admin.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: ServiceAdminApplication
+ * @Package: com.wb.admin.service
+ * @Description: the service admin application entry
+ * @Author：[bo.wang]
+ * @Date: 2021/3/5
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ServiceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAdminApplication.class, args);
+    }
+}
---
+++ wb-module-admin/wb-service-admin/src/main/java/com/wb/admin/service/modules/user/SysUserController.java
@@ -0,0 +1,19 @@
+package com.wb.admin.service.modules.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: SysUserController
+ * @Package: com.wb.service.admin.modules.user
+ * @Description: the sys user controller
+ * @Author：[bo.wang]
+ * @Date: 2021/3/5
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping(value = "user/")
+public class SysUserController {
+
+
+}
